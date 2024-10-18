c0b9a7b823badbbb92f2b0a470d372eaf3e36eed
Marc <elmarcdesempre@gmail.com>
2024-10-18 20:59:01 +0200

    [FIX] Remove unnecessary document accesses

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -17,80 +17,6 @@ import { createElementWithClass } from '../utils/helper.js';
 
 import '../components/pokeball-loader/pokeball-loader.js';
 
-/** 
- * DOM element that contains the Pokémon cards.
- * @type {HTMLElement}
- */
-const cardsContainer = document.querySelector('.content__cards');
-
-/** 
- * Button for loading more data.
- * @type {HTMLElement}
- */
-const loadMoreButton = document.querySelector('.content__button');
-
-/**
- * Search input field.
- * @type {HTMLElement}
- */
-const searchInput = document.querySelector('.header__search-input');
-
-/**
- * Search dropdown.
- * @type {HTMLElement}
- */
-const searchDropdown = document.querySelector('.header__search-dropdown');
-
-/**
- * Button to show filters on mobile.
- * @type {HTMLElement}
- */
-const filterButton = document.querySelector('.main__filter-button');
-
-/**
- * Button to show filters on mobile.
- * @type {HTMLElement}
- */
-const crossButton = document.querySelector('.sidebar__close-button');
-
-/**
- * Button to reset search bar.
- * @type {HTMLElement}
- */
-const resetSearchBox = document.querySelector('.header__search-cross');
-
-/**
- * Button to reset all filters.
- * @type {HTMLElement}
- */
-const resetAllButton = document.getElementById('reset-all-button');
-
-/**
- * Button to reset color filter.
- * @type {HTMLElement}
- */
-const resetColorButton = document.getElementById('reset-color-button');
-
-/**
- * Button to reset type filter.
- * @type {HTMLElement}
- */
-const resetTypeButton = document.getElementById('reset-type-button');
-
-/**
- * Button to reset gender filter.
- * @type {HTMLElement}
- */
-const resetGenderButton = document.getElementById('reset-gender-button');
-
-/** 
- * DOM element for displaying a "No Pokémons found" message.
- * @type {HTMLElement}
- */
-const noResultsMessage = createElementWithClass('div', 'content__no-results');
-noResultsMessage.textContent = 'No Pokémons found...';
-cardsContainer.parentElement.appendChild(noResultsMessage);
-
 /**
  * Array to hold the filtered Pokémon data.
  * @type {Array<Object>}
@@ -133,7 +59,66 @@ let colorMap = new Map();
  */
 let genderMap = new Map();
 
-const loader = document.querySelector('pokeball-loader');
+/**
+ * @const {HTMLElement} header - The header container element.
+ */
+const headerContainer = document.getElementById('header');
+
+/**
+ * @const {Object} headerElms - Elements within the header.
+ * @property {HTMLElement} headerElms.searchInput - Input field for search in the header.
+ * @property {HTMLElement} headerElms.searchDropdown - Search dropdown in the header.
+ * @property {HTMLElement} headerElms.resetSearchBox - Button to reset the search in the header.
+ */
+const headerElms = {
+    searchInput: headerContainer.querySelector('.header__search-input'),
+    searchDropdown: headerContainer.querySelector('.header__search-dropdown'),
+    resetSearchBox: headerContainer.querySelector('.header__search-cross')
+};
+
+/**
+ * @const {HTMLElement} sidebar - The sidebar container element.
+ */
+const sidebarContainer = document.getElementById('sidebar');
+
+/**
+ * @const {Object} sidebarElms - Elements within the sidebar.
+ * @property {HTMLElement} sidebarElms.crossButton - Button to close the sidebar.
+ * @property {HTMLElement} sidebarElms.resetAllButton - Button to reset all filters in the sidebar.
+ * @property {HTMLElement} sidebarElms.resetColorButton - Button to reset the color filter in the sidebar.
+ * @property {HTMLElement} sidebarElms.resetTypeButton - Button to reset the type filter in the sidebar.
+ * @property {HTMLElement} sidebarElms.resetGenderButton - Button to reset the gender filter in the sidebar.
+ */
+const sidebarElms = {
+    crossButton: sidebarContainer.querySelector('.sidebar__close-button'),
+    resetAllButton: sidebarContainer.querySelector('#reset-all-button'),
+    resetColorButton: sidebarContainer.querySelector('#reset-color-button'),
+    resetTypeButton: sidebarContainer.querySelector('#reset-type-button'),
+    resetGenderButton: sidebarContainer.querySelector('#reset-gender-button')
+};
+
+/**
+ * @const {HTMLElement} mobileFilterButton - Button to show filters on mobile devices.
+ */
+const mobileFilterButton = document.querySelector('.main__filter-button');
+
+/**
+ * @const {HTMLElement} content - The content container element.
+ */
+const contentContainer = document.getElementById('content');
+
+/**
+ * @const {Object} contentElms - Elements within the content container.
+ * @property {HTMLElement} contentElms.cardsContainer - Container for Pokémon cards.
+ * @property {HTMLElement} contentElms.loadMoreButton - Button to load more Pokémon cards.
+ * @property {HTMLElement} contentElms.loader - Pokéball loader shown during loads.
+ */
+const contentElms = {
+    cardsContainer: contentContainer.querySelector('.content__cards'),
+    loadMoreButton: contentContainer.querySelector('.content__button'),
+    noResultsMessage: contentContainer.querySelector('.content__no-results'),
+    loader: contentContainer.querySelector('pokeball-loader')
+};
 
 
 /**
@@ -149,7 +134,7 @@ const renderCards = (pokemonList) => {
         card.setAttribute('name', pokemon.name);
         card.setAttribute('image', pokemon.image);
         card.setAttribute('url', pokemon.url);
-        cardsContainer.appendChild(card);
+        contentElms.cardsContainer.appendChild(card);
     });
 };
 
@@ -160,19 +145,19 @@ const renderCards = (pokemonList) => {
  * @returns {void}
  */
 const loadNextBatch = () => {
-    loader.show();
+    contentElms.loader.show();
     const batch = filteredData.slice(currentBatchIndex, currentBatchIndex + batchSize);
     currentBatchIndex += batchSize;
     renderCards(batch);
 
     // Hide load more button if no more data
     if (currentBatchIndex >= filteredData.length) {
-        loadMoreButton.classList.remove('content__button--visible');
+        contentElms.loadMoreButton.classList.remove('content__button--visible');
     } else {
-        loadMoreButton.classList.add('content__button--visible');
+        contentElms.loadMoreButton.classList.add('content__button--visible');
     }
 
-    loader.hide();
+    contentElms.loader.hide();
 };
 
 
@@ -190,17 +175,17 @@ const getSelectedFilters = () => {
     };
 
     // Get selected types
-    document.querySelectorAll(".sidebar__type-checkbox:checked").forEach((checkbox) => {
+    sidebarContainer.querySelectorAll(".sidebar__type-checkbox:checked").forEach((checkbox) => {
         selectedFilters.types.push(checkbox.value);
     });
 
     // Get selected colors
-    document.querySelectorAll(".sidebar__color-checkbox:checked").forEach((checkbox) => {
+    sidebarContainer.querySelectorAll(".sidebar__color-checkbox:checked").forEach((checkbox) => {
         selectedFilters.colors.push(checkbox.value);
     });
 
     // Get selected genders
-    document.querySelectorAll(".sidebar__gender-radio:checked").forEach((radio) => {
+    sidebarContainer.querySelectorAll(".sidebar__gender-radio:checked").forEach((radio) => {
         selectedFilters.genders.push(radio.value);
     });
 
@@ -335,10 +320,10 @@ const filterDataFromSearchBar = async () => {
  * @returns {void}
  */
 function showSearchDropdown(items) {
-    searchDropdown.innerHTML = '';
+    headerElms.searchDropdown.innerHTML = '';
 
     if (items.length === 0) {
-        searchDropdown.classList.remove('header__search-dropdown--visible');
+        headerElms.searchDropdown.classList.remove('header__search-dropdown--visible');
         return;
     }
 
@@ -347,14 +332,14 @@ function showSearchDropdown(items) {
         div.textContent = `#${item.id} ${item.name}`;
         div.className = 'header__search-dropdown-item';
         div.addEventListener('click', () => {
-            searchInput.value = item.name.charAt(0).toUpperCase() + item.name.slice(1);
-            searchDropdown.classList.remove('header__search-dropdown--visible');
+            headerElms.searchInput.value = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+            headerElms.searchDropdown.classList.remove('header__search-dropdown--visible');
             filterData();
         });
-        searchDropdown.appendChild(div);
+        headerElms.searchDropdown.appendChild(div);
     });
 
-    searchDropdown.classList.add('header__search-dropdown--visible');
+    headerElms.searchDropdown.classList.add('header__search-dropdown--visible');
 }
 
 
@@ -364,14 +349,14 @@ function showSearchDropdown(items) {
  * @returns {Promise<void>} - Resolves when the data has been filtered and displayed.
  */
 const filterData = async () => {
-    loader.show();
+    contentElms.loader.show();
 
-    const query = searchInput.value.trim().toLowerCase();
+    const query = headerElms.searchInput.value.trim().toLowerCase();
 
     if (query !== '') {
-        resetSearchBox.classList.add('header__search-cross--visible');
+        headerElms.resetSearchBox.classList.add('header__search-cross--visible');
     } else {
-        resetSearchBox.classList.remove('header__search-cross--visible');
+        headerElms.resetSearchBox.classList.remove('header__search-cross--visible');
     }
 
     const { types, colors, genders } = getSelectedFilters();
@@ -390,45 +375,25 @@ const filterData = async () => {
         filteredData = filteredDataResults.filter(pokemon => pokemon !== null);
 
         currentBatchIndex = 0;
-        cardsContainer.innerHTML = '';
+        contentElms.cardsContainer.innerHTML = '';
 
         if (filteredData.length === 0) {
-            noResultsMessage.classList.add('content__no-results--visible');
-            loadMoreButton.classList.remove('content__button--visible');
+            contentElms.noResultsMessage.classList.add('content__no-results--visible');
+            contentElms.loadMoreButton.classList.remove('content__button--visible');
         } else {
-            noResultsMessage.classList.remove('content__no-results--visible');
+            contentElms.noResultsMessage.classList.remove('content__no-results--visible');
             loadNextBatch();
         }
     } catch (error) {
         console.error('[filterData] Error filtering data:', error);
-        noResultsMessage.classList.add('content__no-results--visible');
+        contentElms.noResultsMessage.classList.add('content__no-results--visible');
     } finally {
-        loader.hide();
+        contentElms.loader.hide();
     }
 };
 
 
-/**
- * Loads the initial data for Pokémon, types, colors, and genders, then applies the initial filter.
- * 
- * @returns {Promise<void>} - Resolves when the data has been loaded and the initial filter has been applied.
- */
-const loadInitialData = async () => {
-    loader.show();
 
-    try {
-        pokemonData = await apiService.fetchPokemonData();
-        filterData();
-
-        typeMap = new Map(Object.entries(await apiService.fetchPokemonTypes()));
-        colorMap = new Map(Object.entries(await apiService.fetchPokemonColors()));
-        genderMap = new Map(Object.entries(await apiService.fetchPokemonGenders()));
-    } catch (error) {
-        console.error('Error loading initial data:', error);
-    } finally {
-        loader.hide();
-    }
-};
 
 
 /**
@@ -448,7 +413,7 @@ const resetAllFilters = async () => {
  * @returns {void} - This function does not return any value.
  */
 const resetColorFilter = async () => {
-    document.querySelectorAll(".sidebar__color-checkbox:checked").forEach((checkbox) => {
+    sidebarContainer.querySelectorAll(".sidebar__color-checkbox:checked").forEach((checkbox) => {
         checkbox.checked = false;
     });
     await filterData();
@@ -460,7 +425,7 @@ const resetColorFilter = async () => {
  * @returns {void} - This function does not return any value.
  */
 const resetTypeFilter = async () => {
-    document.querySelectorAll(".sidebar__type-checkbox:checked").forEach((checkbox) => {
+    sidebarContainer.querySelectorAll(".sidebar__type-checkbox:checked").forEach((checkbox) => {
         checkbox.checked = false;
     });
     await filterData();
@@ -472,7 +437,7 @@ const resetTypeFilter = async () => {
  * @returns {void} - This function does not return any value.
  */
 const resetGenderFilter = async () => {
-    const allRadioButton = document.querySelector(".sidebar__gender-radio[id='all']");
+    const allRadioButton = sidebarContainer.querySelector(".sidebar__gender-radio[id='all']");
     if (allRadioButton) {
         allRadioButton.checked = true;
     }
@@ -486,8 +451,8 @@ const resetGenderFilter = async () => {
  * @returns {void} - This function does not return any value.
  */
 const resetSearchBoxFilter = () => {
-    searchInput.value = '';
-    resetSearchBox.classList.remove('header__search-cross--visible');
+    headerElms.searchInput.value = '';
+    headerElms.resetSearchBox.classList.remove('header__search-cross--visible');
     filterData();
 };
 
@@ -497,8 +462,7 @@ const resetSearchBoxFilter = () => {
  * @returns {void} - This function does not return any value.
  */
 const showFilters = () => {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.add('sidebar--visible');
+    sidebarContainer.classList.add('sidebar--visible');
 };
 
 /**
@@ -507,31 +471,61 @@ const showFilters = () => {
  * @returns {void} - This function does not return any value.
  */
 const closeFilters = () => {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.remove('sidebar--visible');
+    sidebarContainer.classList.remove('sidebar--visible');
 };
 
 
 // Set up the event to load more data when the button is clicked.
-loadMoreButton.addEventListener('click', loadNextBatch);
+contentElms.loadMoreButton.addEventListener('click', loadNextBatch);
 
 // Set up the event to filter data when the search input changes.
-searchInput.addEventListener('input', filterDataFromSearchBar);
+headerElms.searchInput.addEventListener('input', filterDataFromSearchBar);
 
 // Add event listeners for the buttons
-resetAllButton.addEventListener('click', resetAllFilters);
-resetColorButton.addEventListener('click', resetColorFilter);
-resetTypeButton.addEventListener('click', resetTypeFilter);
-resetGenderButton.addEventListener('click', resetGenderFilter);
-resetSearchBox.addEventListener('click', resetSearchBoxFilter);
-filterButton.addEventListener('click', showFilters);
-crossButton.addEventListener('click', closeFilters);
+sidebarElms.resetAllButton.addEventListener('click', resetAllFilters);
+sidebarElms.resetColorButton.addEventListener('click', resetColorFilter);
+sidebarElms.resetTypeButton.addEventListener('click', resetTypeFilter);
+sidebarElms.resetGenderButton.addEventListener('click', resetGenderFilter);
+
+
+headerElms.resetSearchBox.addEventListener('click', resetSearchBoxFilter);
+mobileFilterButton.addEventListener('click', showFilters);
+sidebarElms.crossButton.addEventListener('click', closeFilters);
 
 // Set up the event to filter data when any checkbox or radio button changes.
-document.querySelector('.sidebar__main-fieldset').addEventListener('change', filterData);
+sidebarContainer.querySelector('.sidebar__main-fieldset').addEventListener('change', filterData);
+
+
+/**
+ * Loads the initial data for Pokémon, types, colors, and genders, then applies the initial filter.
+ * 
+ * @returns {Promise<void>} - Resolves when the data has been loaded and the initial filter has been applied.
+ */
+const loadInitialData = async () => {
+    contentElms.loader.show();
+
+    try {
+        pokemonData = await apiService.fetchPokemonData();
+        filterData();
+
+        typeMap = new Map(Object.entries(await apiService.fetchPokemonTypes()));
+        colorMap = new Map(Object.entries(await apiService.fetchPokemonColors()));
+        genderMap = new Map(Object.entries(await apiService.fetchPokemonGenders()));
+    } catch (error) {
+        console.error('Error loading initial data:', error);
+    } finally {
+        contentElms.loader.hide();
+    }
+};
+
+
+const initializeComponent = async () => {
+    loadInitialData();
+}
+
 
 // Set up the event to load initial data when the document is loaded.
-document.addEventListener('DOMContentLoaded', loadInitialData);
+document.addEventListener('DOMContentLoaded', initializeComponent);
 
 /**
  * Handles click events on the document to manage the visibility of the search dropdown and filters.
@@ -542,7 +536,7 @@ document.addEventListener('DOMContentLoaded', loadInitialData);
  */
 document.addEventListener('click', (event) => {
     if (!event.target.closest('.header__search')) {
-        searchDropdown.classList.remove('header__search-dropdown--visible');
+        headerElms.searchDropdown.classList.remove('header__search-dropdown--visible');
     }
 
     if (!event.target.closest('.main__filter-button') && !event.target.closest('.sidebar')) {
